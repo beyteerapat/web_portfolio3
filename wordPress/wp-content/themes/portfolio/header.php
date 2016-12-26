7cb0c21692e7d3fc5958439f326e1eb53e6fd9e1
beyteerapat <khw.teerapat@gmail.com>
2016-12-26 11:49:14 +0700

    adjust header.php

--- wordPress/wp-content/themes/portfolio/header.php
+++ wordPress/wp-content/themes/portfolio/header.php
@@ -23,5 +23,29 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'portfolio' ); ?></a>
 
+     <header class="header">
+
+        <div class="header__group">
+          <a class="header__logo" href="#">MY PROFILE</a>
+          <div class="header__menu-icon"><i class="fa fa-navicon fa-2x" aria-hidden="true"></i></div>
+          <ul class="header__nav1">
+            <li><a href="#hero"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+            <li><a href="#education"><i class="fa fa-graduation-cap" aria-hidden="true"></i></a></li>
+            <li><a href="#experience"><i class="fa fa-briefcase" aria-hidden="true"></i></a></li>
+            <li><a href="#skill"><i class="fa fa-level-up" aria-hidden="true"></i></a></li>
+            <li><a href="#interest"><i class="fa fa-gratipay" aria-hidden="true"></i></a></li>
+            <li><a href="#contact"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
+          </ul>
+
+          <ul class="header__nav">
+            <li><a href="#education"><i class="fa fa-graduation-cap" aria-hidden="true"></i></a></li>
+            <li><a href="#experience"><i class="fa fa-briefcase" aria-hidden="true"></i></a></li>
+            <li><a href="#skill"><i class="fa fa-level-up" aria-hidden="true"></i></a></li>
+            <li><a href="#interest"><i class="fa fa-gratipay" aria-hidden="true"></i></a></li>
+            <li><a href="#contact"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
+          </ul>
+        </div>
+
+      </header>
 
 
